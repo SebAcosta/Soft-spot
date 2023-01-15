a22cc0aee863e873e69ce50bff4918f62da8739d
Alda <caldair99@gmail.com>
2023-01-15 16:30:48 -0600

    AgregarArticulo screen and db routes working

--- Screens/AgregarArticulo.js
+++ Screens/AgregarArticulo.js
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, TextInput, TouchableNativeFeedback, View, DatePickerAndroid, Alert, Button} from 'react-native';
+import { StyleSheet, Text, SafeAreaView, TextInput, View, Alert, TouchableOpacity} from 'react-native';
 import { SelectList } from 'react-native-dropdown-select-list'
 import React, { useState } from 'react';
 import {Ionicons} from '@expo/vector-icons';
@@ -13,8 +12,6 @@ export default function AgregarArticulo(){
     {Key:'3', value:'3'},
     {Key:'4', value:'4'},
   ];
-
-  const [error, setError] = useState(null);
 
   const initArticulo = {
     nombreArticulo: '',
@@ -60,26 +57,31 @@ export default function AgregarArticulo(){
   //Crear en la BD
   async function createArticulo(){
     if(articulo.nombreArticulo === ''){
-      setError('Asignar un nombre de artículo es obligatorio');
+      Alert.alert(
+        'Error',
+        'Asignar un nombre de artículo es obligatorio.',
+        [{text:'OK'}]
+      )
       return;
     }
     try{
       const db = await getDbConnection();
       await insertArticulo(db, articulo.nombreArticulo, articulo.descArt, articulo.cantidad, articulo.cantidadCrit, articulo.precio);
       Alert.alert(
-        'SIUUUUUUUUUUUU',
         'Artículo creado',
-        [
-          {
-            text: 'Ok',
-            onPress: () => navigation.navigate('Artículos')
-          }
-        ],
-        {cancelable: false}
+        `"${articulo.nombreArticulo}" agregado con éxito.`,
+        [{
+            text: 'Ok'
+            //onPress: AGREGAR FUNCION QUE REGRESE A LA PANTALLA DE INICIO
+          }]
       );
-      db.close();
+      db.closeAsync();
     }catch (e){
-      setError(`PERO QUE DICEEEEEES: ${e.message}`)
+      Alert.alert(
+        'Error al crear artículo',
+        `${e.message}`,
+        [{text: 'ok'}]
+      );
     }
   }
 
@@ -93,12 +95,12 @@ export default function AgregarArticulo(){
 
       <TextInput style={styles.input} placeholder="Nombre*" onChangeText={handleNombre} value={articulo.nombreArticulo}/>
       <TextInput style={styles.input} placeholder="Descripción" onChangeText={handleDesc} value={articulo.descArt}/>
-      <TextInput style={styles.input} placeholder="Cantidad" onChangeText={handleCant} value={articulo.cantidad}/>
-      <TextInput style={styles.input} placeholder="Cantidad critica" onChangeText={handleCrit} value={articulo.cantidadCrit}/>
-      <TextInput style={styles.input} placeholder="Precio" onChangeText={handlePrecio} value={articulo.precio}/>
+      <TextInput style={styles.input} placeholder="Precio" onChangeText={handlePrecio} value={articulo.precio} keyboardType = 'numeric'/>
+      <TextInput style={styles.input} placeholder="Cantidad" onChangeText={handleCant} value={articulo.cantidad} keyboardType = 'numeric'/>
+      <TextInput style={styles.input} placeholder="Cantidad crítica" onChangeText={handleCrit} value={articulo.cantidadCrit} keyboardType = 'numeric'/>
       <Text style={styles.text}>Cuando este articulo llegue a la cantidad critica, se le notificará</Text>
 
-      <Text style={styles.eti}>Etiqueta [Opcional]</Text>
+      <Text style={styles.eti}>Etiqueta (opcional)</Text>
       <SelectList 
         data={data}
         setSelected={setSelected}
@@ -109,14 +111,9 @@ export default function AgregarArticulo(){
         placeholder="Ninguna"
       />
 
-      {/* <TouchableNativeFeedback>
-        <View style={styles.guardar}>
-          <Text style={styles.Tguardar}>GUARDAR</Text>
-        </View>
-			</TouchableNativeFeedback> */}
-
-      <Button title='GUARDAR' onPress={createArticulo} />
-      {error && <Text>{error}</Text>}
+      <TouchableOpacity title='GUARDAR' onPress={createArticulo} style={styles.guardar}>
+        <Text style={styles.Tguardar}>GUARDAR</Text>
+      </TouchableOpacity>
 
     </SafeAreaView>
   );
@@ -144,6 +141,7 @@ const styles = StyleSheet.create({
   },
   guardar:{
 		backgroundColor:'#77DD77',
+    text: 'white',
 		width:110,
     height:36,
 		justifyContent:'center',
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,47 +1,47 @@
-import { enablePromise, openDatabase } from "react-native-sqlite-storage";
+import * as SQLite from 'expo-sqlite';
 
-// Para poder trabajar con promesas
-enablePromise(true);
 //Nombre de la base de datos
 const MY_DATABASE = 'soft-spot.db';
 
 
 //Creamos conexión
 export async function getDbConnection(){
-    const db = await openDatabase({name: MY_DATABASE, location: 'default'});
+    const db = await SQLite.openDatabase({name: MY_DATABASE});
+    console.log("Conectando a la BDD...")
     return db;
 }
 
 //Creamos las tablas
 export async function createTables(db){
-    const query = `CREATE TABLE IF NOT EXIST articulo (idArticulo int PRIMARY KEY AUTOINCREMENT, nombreArticulo varchar(50), 
-    descArt varchar(200), cantidad int(3), cantidadCrit int(3), precio double(6,2));`;
-    console.log("Tabla Artículos creada")
-    return db.executeSql(query);
+    const query = `CREATE TABLE IF NOT EXISTS articulo (idArticulo INTEGER PRIMARY KEY AUTOINCREMENT, nombreArticulo VARCHAR(50), descArt VARCHAR(200), cantidad INTEGER(3), cantidadCrit INTEGER(3), precio DOUBLE(6,2))`;
+    console.log("Tablas creadas");
+    return db.transaction(tx => {tx.executeSql(query)});
 }
 
 //Inicializamos la BD
 export async function initDatabase(){
     const db = await getDbConnection();
     await createTables(db);
-    db.close();
+    console.log("DB inicializada")
+    db.closeAsync();
 }
 
 //Inserts para ARTICULOS
 export async function insertArticulo(db, nombreArticulo, descArt, cantidad, cantidadCrit, precio){
-    const insertQuery = `INSERT INTO articulo (nombreArticulo,descArt,cantidad,cantidadCrit,precio) 
-    VALUES ('${nombreArticulo}','${descArt}','${cantidad}','${cantidadCrit}','${precio}');`
-    console.log(insertQuery);
-    return db.executeSql(insertQuery);
+    const insertQuery = `INSERT INTO articulo (nombreArticulo,descArt,cantidad,cantidadCrit,precio) VALUES ('${nombreArticulo}','${descArt}','${cantidad}','${cantidadCrit}','${precio}');`
+    console.log(`Insertando en articulos: '${nombreArticulo}','${descArt}','${cantidad}','${cantidadCrit}','${precio}'`);
+    return db.transaction(tx=> {tx.executeSql(insertQuery)});
 }
+
 //Obtener ARTICULOS
-export async function getArticulos(db){
-    const articulos = [];
-    const results = await db.executeSql('SELECT * FROM articulo');
-    results.forEach(function (resultSet) {
-        for(let index =0;index<resultSet.rows.length; index++){
-            articulos.push(resultSet.rows.item(index));
-        }
-    });
-    return articulos;
-}
+// export async function getArticulos(db){
+//     console.log("Obteniendo articulos...")
+//     const articulosLista = [];
+//     const results = await db.transaction(tx => {tx.executeSql('SELECT * FROM articulo', null,
+//         (txObj, resultSet) => setArticulos(resultSet.rows._array),
+//         (txObj, error) => console.log(error)
+//     )});
+//     console.log(articulos);
+
+//     return articulosLista;
+// }
